c7825ac76c675ef58e01bf84f9d2f454be38da23
Leandro74377 <leandrobaque4@gmail.com>
2026-05-03 00:42:56 -0500

    Refactor database connection variables

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
-$user = "root";     // Usuario por defecto de XAMPP
-$pass = "";         // Contraseña por defecto (vacía)
+$user = "root";     
+$pass = "";         
 $db   = "sistema_db";
 
 $conexion = mysqli_connect($host, $user, $pass, $db);
 
-// Verificar si funciona
+
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
